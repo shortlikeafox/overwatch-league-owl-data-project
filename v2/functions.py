c403f2d49e3ce15754f06b04adfa7ce98cdd1091
shortlikeafox <62516729+shortlikeafox@users.noreply.github.com>
2020-05-01 10:42:37 -0500

    finished v2

--- v2/functions.py
+++ v2/functions.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
 
 #Helpful functions for the project
 
@@ -301,3 +304,217 @@ def find_last_n_results(master_df, n):
             master_df['t2_win_percent_last_20'][mask] = t2_win_percent    
     
     return master_df
+
+
+#Input: American Odds, and Probability of a Winning Bet
+#Output: Bet EV based on a $100 bet
+def get_bet_ev(odds, prob):
+    if odds>0:
+        return ((odds * prob) - (100 * (1-prob)) )
+    else:
+        return ((100 / abs(odds))*100*prob - (100 * (1-prob)))
+
+
+#Input: American Odds
+#Output: Profit on a successful bet
+def get_bet_return(odds):
+    if odds>0:
+        return odds
+    else:
+        return (100 / abs(odds))*100
+    
+
+#Input DF must have these columns:
+#t1_odds (American)
+#t2_odds (American)
+#t1_prob (0->1)
+#t2_prob (0->1)
+#winner (0 or 1)
+#OUTPUT: Profit per bet (based on bet of $100)
+
+def get_ev_from_df(ev_df, print_stats = False):
+    num_matches = 0
+    num_bets = 0
+    num_wins = 0
+    num_losses= 0
+    num_under= 0
+    num_under_losses = 0
+    num_under_wins = 0
+    num_even = 0
+    num_even_losses = 0
+    num_even_wins = 0
+    num_fav = 0
+    num_fav_wins = 0
+    num_fav_losses = 0
+    profit = 0
+    profit_per_bet = 0
+    profit_per_match = 0    
+
+    for index, row in ev_df.iterrows():
+        num_matches = num_matches+1
+        t1_bet_ev = get_bet_ev(row['t1_odds'], row['t1_prob'])
+        #print(f"ODDS:{row['t1_odds']} PROB: {row['t1_prob']} EV: {t1_bet_ev}")
+        t2_bet_ev = get_bet_ev(row['t2_odds'], row['t2_prob'])
+        #print(f"ODDS:{row['t2_odds']} PROB: {row['t2_prob']} EV: {t2_bet_ev}")
+        #print()
+        
+        t1_bet_return = get_bet_return(row['t1_odds'])
+        t2_bet_return = get_bet_return(row['t2_odds'])
+        
+        
+        if (t1_bet_ev > 0 or t2_bet_ev > 0):
+            num_bets = num_bets+1
+
+            
+        if t1_bet_ev > 0:
+            if row['winner'] == 0:
+                num_wins += 1
+                profit = profit + t1_bet_return
+                #print(t1_bet_return)
+            elif row['winner'] == 1:
+                num_losses += 1
+                profit = profit - 100
+            if (t1_bet_return > t2_bet_return):
+                num_under += 1
+                if row['winner'] == 0:
+                    num_under_wins += 1
+                elif row['winner'] == 1:
+                    num_under_losses += 1
+            elif (t1_bet_return < t2_bet_return):
+                num_fav += 1
+                if row['winner'] == 0:
+                    num_fav_wins += 1
+                elif row['winner'] == 1:
+                    num_fav_losses += 1
+            else:
+                num_even += 1
+                if row['winner'] == 0:
+                    num_even_wins += 1
+                elif row['winner'] == 1:
+                    num_even_losses += 1
+
+        if t2_bet_ev > 0:
+            if row['winner'] == 1:
+                num_wins += 1                    
+                profit = profit + t2_bet_return
+            elif row['winner'] == 0:
+                num_losses += 1
+                profit = profit - 100
+            if (t2_bet_return > t1_bet_return):
+                num_under += 1
+                if row['winner'] == 1:
+                    num_under_wins += 1
+                elif row['winner'] == 0:
+                    num_under_losses += 1
+            elif (t2_bet_return < t1_bet_return):
+                num_fav += 1
+                if row['winner'] == 1:
+                    num_fav_wins += 1
+                elif row['winner'] == 0:
+                    num_fav_losses += 1
+            else:
+                num_even += 1
+                if row['winner'] == 1:
+                    num_even_wins += 1
+                elif row['winner'] == 0:
+                    num_even_losses += 1
+            
+    profit_per_bet = profit / num_bets
+    profit_per_match = profit / num_matches
+        
+        
+    if print_stats:
+        print(f"""
+          Number of matches: {num_matches}
+          Number of bets: {num_bets}
+          Number of winning bets: {num_wins}
+          Number of losing bets: {num_losses}
+          Number of underdog bets: {num_under}
+          Number of underdog wins: {num_under_wins}
+          Number of underdog losses: {num_under_losses}
+          Number of Favorite bets: {num_fav}
+          Number of favorite wins: {num_fav_wins}
+          Number of favorite losses: {num_fav_losses}
+          Number of even bets: {num_even}
+          Number of even wins: {num_even_wins}
+          Number of even losses: {num_even_losses}
+          Profit: {profit}
+          Profit per bet: {profit_per_bet}
+          Profit per match: {profit_per_match}
+          
+          """)
+        
+    return (profit_per_bet)
+            
+
+#Input the train df and model and we will return a customer 5x cross validation score based off of expected value
+#t1_odds and t2_odd MUST be the last 2 columns or this will break.
+
+def custom_cv_eval(df, m):
+    #We need to split away the winner...
+    y = df['winner_label'].copy()
+    #display(y)
+    X = df.drop('winner_label', axis=1)
+    
+    ##We need a numpy array it seems like...
+    X = np.array(X)
+    y = np.array(y)
+    #display(X)
+    running_total = 0
+    count=1
+    kf = KFold(n_splits=5, shuffle=True, random_state=75)
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        #print(test_index)
+        m.fit(X_train, y_train)
+        probs=m.predict_proba(X_test)
+        #We need to prep the dataframe to evaluate....
+        #X_odds = X_test[['t1_odds', 't2_odds']]
+        #print(X_test)
+        #print(X_test[:, -1])
+        #print(X_test[:, -2])
+        X_odds = list(zip(X_test[:, -2], X_test[:, -1], probs[:, 0], probs[:, 1], y_test))
+        ev_prepped_df = pd.DataFrame(X_odds, columns=['t1_odds', 't2_odds', 't1_prob', 't2_prob', 'winner'])
+        #display(temp_df)
+        #print(f"{count}: {get_ev_from_df(ev_prepped_df, print_stats = False)}")
+        count=count+1
+        running_total = running_total + get_ev_from_df(ev_prepped_df, print_stats = False)
+    return running_total
+
+
+
+def get_best_features(features, model, df, current_features, scale=False):
+    best_feature = ""
+    winner_labels = df['winner_label'].copy()
+    initial_df = df[current_features]
+    #display(initial_df)
+    #display(winner_labels)
+    
+    best_score = custom_cv_eval(df[current_features], model)
+    best_feature = ""
+    
+    print(f"Current best score is: {best_score}")
+    for f in features:
+        if f not in current_features:
+            new_features = [f] + current_features
+            df_sel=df[new_features]
+            if scale == True:
+                sc = StandardScaler()
+                df_sel = sc.fit_transform(df_sel)
+            new_score = custom_cv_eval(df_sel, model)
+            #print(f"Total score for {f} is: {new_score}")
+            if new_score > best_score:
+                best_score = new_score
+                best_feature = f
+            #print()
+    #Keep running until we don't improve
+    if best_feature != "":
+        print(f"The best feature was {best_feature}.  It scored {best_score}")
+        current_features = [best_feature] + current_features
+        
+        return(get_best_features(features, model, df, current_features, scale))
+    else:
+        print("NO IMPROVEMENT")
+        print(f"FINAL BEST SCORE: {best_score}")
+        return current_features
