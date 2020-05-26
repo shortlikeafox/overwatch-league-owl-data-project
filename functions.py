4d5d57f140c16825ea0f13dd50e8aa73e24e3fed
shortlikeafox <62516729+shortlikeafox@users.noreply.github.com>
2020-05-25 21:13:34 -0500

    working on models

--- functions.py
+++ functions.py
@@ -518,3 +518,41 @@ def get_best_features(features, model, df, current_features, scale=False):
         print("NO IMPROVEMENT")
         print(f"FINAL BEST SCORE: {best_score}")
         return current_features
+    
+    
+    
+    
+#INPUT: 
+#df: The df to be evaluated
+#m: The model to use
+# labels: The labels
+#odds: The odds
+#min_ev: The minimum EV to place a bet
+def custom_cv_eval_v2(df, m, labels, odds, min_ev=0):
+    X = np.array(df)
+    y = np.array(labels)
+    odds = np.array(odds)
+    running_total = 0
+    count=1
+    kf = KFold(n_splits=5, shuffle=True, random_state=75)
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        odds_train, odds_test = odds[train_index], odds[test_index]
+        #print(test_index)
+        m.fit(X_train, y_train)
+        probs=m.predict_proba(X_test)
+        #We need to prep the dataframe to evaluate....
+        #X_odds = X_test[['t1_odds', 't2_odds']]
+        #print(X_test)
+        #print(X_test[:, -1])
+        #print(X_test[:, -2])
+        X_odds = list(zip(odds_test[:, -2], odds_test[:, -1], probs[:, 0], probs[:, 1], y_test))
+        ev_prepped_df = pd.DataFrame(X_odds, columns=['t1_odds', 't2_odds', 't1_prob', 't2_prob', 'winner'])
+        #display(temp_df)
+        #print(f"{count}: {get_ev_from_df(ev_prepped_df, print_stats = False)}")
+        count=count+1
+        running_total = running_total + get_ev_from_df(ev_prepped_df, print_stats = False, min_ev = min_ev)
+        #display(ev_prepped_df)
+    
+    return running_total    
